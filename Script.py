f110225d5a9e36c503c612bab109cd4675f436b2
SuNeEsH-_-Rr0 <suneesh001dz@gmail.com>
2024-12-21 15:45:54 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -3,35 +3,22 @@ class script(object):
 𝖨𝗆 𝖺𝗇 𝖺𝗎𝗍𝗈 𝖿𝗂𝗅𝗍𝖾𝗋 𝖻𝗈𝗍 𝗐𝗁𝗂𝖼𝗁 𝖼𝖺𝗇 𝗉𝗋𝗈𝗏𝗂𝖽𝖾 𝗆𝗈𝗏𝗂𝖾𝗌 𝗂𝗇 𝗒𝗈𝗎𝗋 𝗀𝗋𝗈𝗎𝗉𝗌.
 + 𝖠𝖽𝖽 𝖬𝖾 𝖳𝗈 𝖸𝗈𝗎𝗋 𝖦𝗋𝗈𝗎𝗉 + 𝖺𝗇𝖽 𝗉𝗋𝗈𝗆𝗈𝗍𝖾 𝗆𝖾 𝖺𝗌 𝖺𝖽𝗆𝗂𝗇 𝗍𝗈 𝗅𝖾𝗍 𝗆𝖾 𝗀𝖾𝗍 𝗂𝗇 𝖺𝖼𝗍𝗂𝗈𝗇.
 
-<blockquote>©️ Maintained By @im_goutham_josh</blockquote>"""
+<blockquote>©️ Maintained By @d_Vittal_911545</blockquote>"""
     HELP_TXT = """
-    🙋🏻‍♂️   Hellooo  {} 🤓
+    <blockquote>🙋🏻‍♂️   Hellooo  {} 🤓</blockquote>
 
-<blockquote>○ Available Commands</blockquote>
-     
- /start - Check I'm Alive..
- /ping - check ping
- /usage - usage of bot
- /status - Bot Status
- /info - User info 
- /id - User id
- /stats - Db status  
- /broadcast - Broadcast (owner only)
-
-○ Notice 📙:-
-○ Dont Spam Me...🙂
+<b>○ Available Commands..🔮</b>
 """
     ABOUT_TXT = """<b><blockquote>◎ Nᴀᴍᴇ: ᴋᴜᴛᴛᴜ ʙᴏᴛ™</blockquote>
-◎ Cʀᴇᴀᴛᴏʀ: <a href=https://t.me/wudixh13>Goutham SER</a>
+◎ Cʀᴇᴀᴛᴏʀ: <a href=https://t.me/d_Vittal_911545>DACCHU</a>
 ◎ Lᴀɴɢᴜᴀɢᴇ: Pʏᴛʜᴏɴ 3
 ◎ Dᴀᴛᴀ Bᴀsᴇ: Mᴏɴɢᴏ DB
 ◎ Bᴏᴛ Sᴇʀᴠᴇʀ: KoYeb</b>"""
     SOURCE_TXT = """<b>NOTE:</b>
--<blockquote> ᴋᴜᴛᴛᴜ ʙᴏᴛ™ is a open source project. </blockquote>
-- Source - <ahref=https://github.com/GouthamSER>Click Here😂</a>
+- Source - <ahref=https://github.com>Click Here😂</a>
 
 <b>DEVS:</b>
--<blockquote> <a href=https://t.me/wudixh13/4>Gᴏᴜᴛʜᴀᴍ Josh ✅</a></blockquote>"""
+-<blockquote> <a href=https://t.me/d_Vittal_911545>DACCHU ✅</a></blockquote>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 <b>NOTE:</b>
@@ -121,14 +108,14 @@ Added By - {}
 FɪʟᴇSɪᴢᴇ : {file_size}
 
 ╔════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╗
-▫️<a href=https://t.me/wudixh> ᴇʟᴅᴏʀᴀᴅᴏ </a>
+▫️<a href=https://t.me/> ᴇʟᴅᴏʀᴀᴅᴏ </a>
 ╚════ ᴊᴏɪɴ ᴡɪᴛʜ ᴜs ════╝</b>"""
 
     
     RESTART_GC_TXT = """
 <b>𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 !</b>
 Kuttu Bot
-**@im_goutham_josh**
+**@d_Vittal_911545**
 
 📅 𝖣𝖺𝗍𝖾 : <code>{}</code>
 ⏰ 𝖳𝗂𝗆𝖾 : <code>{}</code>
@@ -170,7 +157,7 @@ Try reading the instruction below 👇🏼
     OLD_MES=""" Eᴅᴀᴀ Mᴏɴᴇʜ 𝐘𝐨𝐮 𝐚𝐫𝐞 𝐮𝐬𝐢𝐧𝐠 𝐨𝐧𝐞 𝐨𝐟 𝐦𝐲 𝐨𝐥𝐝 𝐦𝐞𝐬𝐬𝐚𝐠𝐞𝐬🤔, 𝐩𝐥𝐞𝐚𝐬𝐞 𝐬𝐞𝐧𝐝 𝐭𝐡𝐞 𝐫𝐞𝐪𝐮𝐞𝐬𝐭 𝐚𝐠𝐚𝐢𝐧"""
     
     MOV_NT_FND="""<b>Eᴅᴀᴀ Mᴏɴᴇʜ Tʜɪs Mᴏᴠɪᴇ Is Nᴏᴛ Yᴇᴛ Rᴇᴀʟᴇsᴇᴅ Oʀ Aᴅᴅᴇᴅ Tᴏ DB</b>
-<blockquote>Report To ADMIN - @im_goutham_josh</blockquote>
+<blockquote>Report To ADMIN - @d_Vittal_911545</blockquote>
 """
     RESTART_TXT = """
 <b><u>𝖡𝗈𝗍 𝖱𝖾𝗌𝗍𝖺𝗋𝗍𝖾𝖽 ✅</u></b>"""
