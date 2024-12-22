776dbe14f468df05bf594afce363c1dd51030bf6
SuNeEsH-_-Rr0 <suneesh001dz@gmail.com>
2024-12-22 11:43:58 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -248,8 +248,8 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📣 UPDATES CHANNEL 📣", url='https://t.me/DVITTALKANNADAMOVIE')]]),
         protect_content=True if pre == 'filep' else False,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("📣 UPDATES CHANNEL 📣", url='https://t.me/DVITTALKANNADAMOVIE')]])
         )
                     
 
